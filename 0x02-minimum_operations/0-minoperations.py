0220ab7e1afd4016e13dbe2775e7be5750f64fce
math198512 <thoraichi@gmail.com>
2024-08-15 19:25:56 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,34 @@
 #!/usr/bin/python3
 """"""
+import math
 
 
 def minOperations(n):
-    pass
+    """"""
+    def primeFactors(n):
+        # Print the number of two's that divide n
+        primes = []
+        while n % 2 == 0:
+            primes.append(2)
+            n = n // 2
+
+        # n must be odd at this point
+        # so a skip of 2 ( i = i + 2) can be used
+        for i in range(3, int(math.sqrt(n))+1, 2):
+
+            # while i divides n , print i ad divide n
+            while n % i == 0:
+                primes.append(i)
+                n = n // i
+
+        # Condition if n is a prime
+        # number greater than 2
+        if n > 2:
+            primes.append(n)
+        return primes
+
+    if n <= 0:
+        return 0
+    else:
+        result = sum(primeFactors(n))
+        return result
