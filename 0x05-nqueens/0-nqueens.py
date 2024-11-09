0e3909062df58d4ec5084233b2e845f1c2f05d27
math198512 <thoraichi@gmail.com>
2024-11-09 22:04:46 +0100

    0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -33,5 +33,15 @@ def n_queens(n):
     rec(board, 0)
 
 
-n = int(sys.argv[1])
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+try:
+    n = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    sys.exit(1)
+if n < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 n_queens(n)
