00a7dbab59445cb5db03210debb7e70061566cd4
math198512 <thoraichi@gmail.com>
2024-11-09 21:58:29 +0100

    0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""algorithm to solve N Queens problem"""
+
+import sys
+
+
+def is_safe(board, i, j, placed_queens):
+    return not any(
+                    i == i_
+                    or j == j_
+                    or abs(i - i_) == abs(j - j_) for i_, j_ in placed_queens)
+
+
+def rec(board, i, placed_queens=None):
+    n = len(board)
+    if i == n and placed_queens is not None:
+        print(placed_queens)
+        return 1
+
+    if placed_queens is None:
+        placed_queens = []
+    for j in range(n):
+        if is_safe(board, i, j, placed_queens):
+            board[i][j] = 1
+            placed_queens.append([i, j])
+            rec(board, i+1, placed_queens[:])
+            placed_queens.pop()
+            board[i][j] = 0
+
+
+def n_queens(n):
+    board = [[0]*n for _ in range(n)]
+    rec(board, 0)
+
+
+n = int(sys.argv[1])
+n_queens(n)
