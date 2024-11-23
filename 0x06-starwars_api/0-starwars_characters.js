31d1369ae9e6dead9074e69b60df17744c9687ff
math198512 <thoraichi@gmail.com>
2024-11-23 18:13:39 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api/films/3/';
+const BASE_URL = 'https://swapi-api.hbtn.io/api/films/';
 
+const filmId = process.argv[2];
 
+const API_URL = BASE_URL + filmId;
 request(API_URL, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        let json = JSON.parse(body);
-        const charac_list = json.characters;  // Handle the response body
-        const promises = charac_list.map(url => {
-            return new Promise((resolve, reject) => {
-                request(url, (error1, response1, body1) => {
-                    if (!error1 && response1.statusCode == 200) {
-                        resolve(JSON.parse(body1).name);
-                    } else {
-                        reject(error1);
-                    }
-                });
-            });
+  if (!error && response.statusCode === 200) {
+    const json = JSON.parse(body);
+    const characList = json.characters; // Handle the response body
+    const promises = characList.map(url => {
+      return new Promise((resolve, reject) => {
+        request(url, (error1, response1, body1) => {
+          if (!error1 && response1.statusCode === 200) {
+            resolve(JSON.parse(body1).name);
+          } else {
+            reject(error1);
+          }
         });
+      });
+    });
 
-        Promise.all(promises)
-            .then(names => {
-                names.forEach(name => console.log(name));
-            })
-            .catch(error => console.error(error));
-    }
-})
+    Promise.all(promises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => console.error(error));
+  }
+});
