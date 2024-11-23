5d185efc3783843632481c49ef29035760f53ecb
math198512 <thoraichi@gmail.com>
2024-11-23 17:07:47 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api/films/3';
-
+const API_URL = 'https://swapi-api.hbtn.io/api/films/3/';
 
 
 request(API_URL, (error, response, body) => {
