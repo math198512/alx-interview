bd13d0dc5b95da5a5de7f7d17b80f84d525e656e
math198512 <thoraichi@gmail.com>
2024-11-23 17:05:09 +0100

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api/films/3';
+
+
+
+request(API_URL, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+        let json = JSON.parse(body);
+        const charac_list = json.characters;  // Handle the response body
+        for (let i = 0; i < charac_list.length; i++) {
+            request(charac_list[i], (error1, response1, body1) => {
+                if (!error1 && response1.statusCode == 200) {
+                    console.log(JSON.parse(body1).name)
+                }
+            })
+        }
+    }
+})
